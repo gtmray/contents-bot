18c97654b55ea9d77dcc93847f7b9f7b6e981450
Rewan Gautam <rewangautam@gmail.com>
2025-01-21 14:42:42 +0545

    Refactor: Read from config file

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,15 +1,23 @@
+import yaml
 import json
 import random
-import logging
+import logging.config
 from dotenv import load_dotenv, find_dotenv
+
 from fetch_article import extract_news_content
 from script_gen import generate_script, generate_prompts, generate_title_desc
+
 from image_gen import generate_images
 from tts.text_to_speech import generate_audio
 from video_creator import create_video_with_audio
 from video_uploader import upload_video, get_authenticated_service
 
-logging.config.fileConfig("../config/logging_config.ini")
+# Load configuration from file
+with open("./src/config/config.yaml", "r") as config_file:
+    config = yaml.safe_load(config_file)
+
+logging.config.fileConfig(config.get("logging_config_file"))
+
 logger = logging.getLogger()
 
 load_dotenv(find_dotenv())
@@ -17,14 +25,13 @@ load_dotenv(find_dotenv())
 
 async def main():
     url = ""
-    output_audio_file = "output_audio.wav"
-
     num_images = 2
     seed = random.randint(0, 100000)
-    comfyui_api_json_path = "../config/flux_dev.json"
-    output_img_folder = "output_imgs"
 
-    output_video_file = "output_video.mp4"
+    title_desc_file = "./src/bot/title_desc.json"
+    output_audio_file = "./src/bot/output_audio.wav"
+    output_img_folder = "./src/bot/output_imgs"
+    output_video_file = "./src/bot/output_video.mp4"
 
     yt_privacy_status = "private"
 
@@ -41,13 +48,15 @@ async def main():
     print("*" * 100)
 
     title_desc = generate_title_desc(script)
+    with open(title_desc_file, "w") as f:
+        f.write(json.dumps(title_desc, indent=4))
     print(f"Title and Description: {title_desc}")
     print("*" * 100)
     generate_audio(script, save_audio=True, output_file=output_audio_file)
     print("*" * 100)
 
     # Generate images based on prompts
-    with open(comfyui_api_json_path) as f:
+    with open(config.get("comfyui_api_json_path")) as f:
         workflow = json.loads(f.read())
     for prompt in prompts:
         # Update the workflow details
